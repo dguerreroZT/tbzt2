e6f35f3b3939c7a1d046482ffda17ae0d6352974
dguerreroZT <dguerrero@zafirosoft.com>
2017-07-31 16:10:05 -0500

    Agregar Listado
    Se agrega listado de Vehiculos

---
+++ TireBits_20/Public/js/Vehiculos.html.js
@@ -0,0 +1,35 @@
+﻿$(function(){
+	//$('.animsition').animsition();
+    
+	// Check Login
+	//function Login(username, nip){
+		spawn(function *(){
+			try{
+				let Vehiculos = yield TireBits.Vehiculos.listado("abcd")
+
+                for (let Vehiculo of Vehiculos){
+                    let finished = yield agregarElementoListado({
+                        div:"lstVehiculos",
+                        data:{
+                            Icon:"local_shipping",
+                            Titulo: "Número Económico: " + Vehiculo.NoEconomico,
+                            Subtitulo1: "Marca: " + Vehiculo.Marca + "; Modelo: " + Vehiculo.Modelo,
+                            Subtitulo2: "Placas:" + Vehiculo.Placas
+                        }
+                    })
+                }
+                
+			}catch(err){
+				console.log(err)
+			}
+
+		})
+	//}
+    
+
+    $("#btnAdd").click(function(){
+		window.location = "editarVehiculo.html"
+	})
+
+
+})
---
+++ TireBits_20/Vehiculos.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="vb" CodeBehind="Vehiculos.asmx.vb" Class="TireBits_20.Vehiculos" %>
